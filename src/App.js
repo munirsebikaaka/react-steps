6ba3090b21bbad1a9537eec8e508726d5fd04e10
munir sebikaaka <munirsebikaaka@gmail.com>
2024-08-19 04:15:55 +0300

    feat: added the styles to the application to give it a good looking interface

--- src/App.js
+++ src/App.js
@@ -1,3 +1,20 @@
 export default function App() {
-  return <div>hello react</div>;
+  return (
+    <div className="container">
+      <div className="numbers">
+        <div className="active">1</div>
+        <div>2</div>
+        <div>3</div>
+      </div>
+      <p className="message">HELLO</p>
+      <div className="btns">
+        <button style={{ backgroundColor: "#7960f2", color: "#fff" }}>
+          previous
+        </button>
+        <button style={{ backgroundColor: "#7960f2", color: "#fff" }}>
+          next
+        </button>
+      </div>
+    </div>
+  );
 }
