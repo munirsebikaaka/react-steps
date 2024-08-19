852f22b0e3b72c4d5524272e5432260db958cbfa
munir sebikaaka <munirsebikaaka@gmail.com>
2024-08-19 07:50:10 +0300

    feat: added the step counter functionallity that checks if the counter is added to go to the next level or the counter is subtracted to go to the previous level

--- src/App.js
+++ src/App.js
@@ -1,17 +1,36 @@
+import { useState } from "react";
+
+const stepM = ["CODESMANN DEV", "ADAM BUSADA", "MOXTECH DEVS"];
+
 export default function App() {
+  let [step, setStep] = useState(1);
+  function previous() {
+    if (step > 1) setStep(step - 1);
+  }
+  function next() {
+    if (step < 3) setStep(step + 1);
+  }
   return (
     <div className="container">
       <div className="numbers">
-        <div className="active">1</div>
-        <div>2</div>
-        <div>3</div>
+        <div className={` ${step >= 1 ? "active" : ""}`}>1</div>
+        <div className={` ${step >= 2 ? "active" : ""}`}>2</div>
+        <div className={` ${step >= 3 ? "active" : ""}`}>3</div>
       </div>
-      <p className="message">HELLO</p>
+      <p className="message">
+        Step {step}: {stepM[step - 1]}
+      </p>
       <div className="btns">
-        <button style={{ backgroundColor: "#7960f2", color: "#fff" }}>
+        <button
+          onClick={previous}
+          style={{ backgroundColor: "#7960f2", color: "#fff" }}
+        >
           previous
         </button>
-        <button style={{ backgroundColor: "#7960f2", color: "#fff" }}>
+        <button
+          onClick={next}
+          style={{ backgroundColor: "#7960f2", color: "#fff" }}
+        >
           next
         </button>
       </div>
